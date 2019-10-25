a29de0de03da74fcb3203e263efd9201b429cb2f
PrawinAyadi <49094521+PrawinAyadi@users.noreply.github.com>
2019-10-25 10:18:58 +0545

    Initial commit

---
+++ app/src/main/java/com/example/listview_prabin/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.listview_prabin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
